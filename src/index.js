54b373ffb5270b695a58db661507993299b2d2fe
Shawn Webster <swolfgangwebster@gmail.com>
2023-04-16 21:15:38 -0500

    ill make it better later

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    let task = e.target.ntd.value
+    e.target.ntd.value = ""
+    let p = document.createElement('p')
+    p.textContent = task
+    document.querySelector('#tasks').appendChild(p)
+  })
 });
